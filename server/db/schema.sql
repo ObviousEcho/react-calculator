2486cd9268116f25e15c8c6498a824e1b0d49a4a
Adia & Indy <107782632+ObviousEcho@users.noreply.github.com>
2023-05-31 19:17:31 -0600

    created schema.sql and connection to database

---
+++ server/db/schema.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS memory_db;
+CREATE DATABASE memory_db;
+
+USE memory_db;
+
+CREATE TABLE memory
+(
+    id INT NOT NULL
+    AUTO_INCREMENT PRIMARY KEY,
+    memory_slot DECIMAL
+);
--- server/server.js
+++ server/server.js
@@ -1,9 +1,22 @@
 const express = require("express");
+require("dotenv").config();
+const mysql = require("mysql2");
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+  console.log(`Connected to the memory_db database.`)
+);
 
 app.listen(PORT, () => {
   console.log(`Server runing on port ${PORT}`);
