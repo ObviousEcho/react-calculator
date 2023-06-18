dfdad3431945c79bef8637aa352610779ecbf426
Adia & Indy <107782632+ObviousEcho@users.noreply.github.com>
2023-06-18 00:57:03 -0600

    created logic to perform basic math

--- client/src/components/layout/Calc.js
+++ client/src/components/layout/Calc.js
@@ -8,7 +8,7 @@ const Calc = () => {
   const symbols = [
     "STO",
     "RCL",
-    "CL",
+    "RST",
     "AC",
     "+/-",
     "%",
@@ -30,17 +30,19 @@ const Calc = () => {
     "=",
   ];
 
-  const [value1, setValue1] = useState("");
+  const [value1, setValue1] = useState(0);
   const [value2, setValue2] = useState(0);
+  const [opr, setOpr] = useState("");
 
   const buttonClickHandler = (e) => {
     let element = e.target;
-    let operator = e.target.innerText;
+    let btnClicked = e.target.innerText;
     const nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const operators = ["/", "*", "+", "-"];
     const func = [
       "STO",
       "RCL",
-      "CL",
+      "RST",
       "AC",
       "+/-",
       "%",
@@ -54,52 +56,83 @@ const Calc = () => {
 
     const numKeys =
       (element.matches("p") || element.matches("div")) &&
-      nums.includes(parseInt(operator));
+      nums.includes(parseInt(btnClicked));
     const oprKeys =
       (element.matches("p") || element.matches("div")) &&
-      func.includes(operator);
+      func.includes(btnClicked);
+
+    const operations = () => {
+      setValue2(value1);
+      setValue1(0);
+    };
+
+    const equals = (x) => {
+      switch (x) {
+        case "/":
+          setValue1((prev) => parseInt(value2) / parseInt(prev));
+          setValue2(0);
+          break;
+        case "*":
+          setValue1((prev) => parseInt(prev) * parseInt(value2));
+          setValue2(0);
+          break;
+        case "-":
+          setValue1((prev) => parseInt(value2) - parseInt(prev));
+          setValue2(0);
+          break;
+        case "+":
+          setValue1((prev) => parseInt(prev) + parseInt(value2));
+          setValue2(0);
+          break;
+        default:
+          console.log(`Learn how to do Math!`);
+      }
+    };
+
+    if (operators.includes(btnClicked)) {
+      setOpr(btnClicked);
+    }
 
     if (numKeys) {
-      setValue1((prev) => prev + operator);
+      setValue1((prev) => prev + parseInt(btnClicked));
     }
     if (oprKeys) {
-      console.log(operator);
-      switch (operator) {
+      switch (btnClicked) {
         case "STO":
           console.log("Test");
           break;
         case "RCL":
           console.log("Test");
           break;
-        case "CL":
-          setValue1("");
+        case "RST":
+          console.log("Test");
           break;
         case "AC":
-          console.log("Test");
-          break;
-        case "+/-":
-          console.log("Test");
-          break;
-        case "%":
-          console.log("Test");
-          break;
-        case "/":
-          console.log("Test");
-          break;
-        case "*":
-          console.log("Test");
-          break;
-        case "-":
           setValue1("");
           break;
+        case "+/-":
+          setValue1((prev) => prev * -1);
+          break;
+        case "%":
+          setValue1((prev) => prev / 100);
+          break;
+        case "/":
+          operations();
+          break;
+        case "*":
+          operations();
+          break;
+        case "-":
+          operations();
+          break;
         case "+":
-          console.log("Test");
+          operations();
           break;
         case "=":
-          console.log("Test");
+          equals(opr);
           break;
         case ".":
-          setValue1((prev) => prev + operator);
+          setValue1((prev) => prev + btnClicked);
           break;
         default:
           console.log(`Learn how to do Math!`);
