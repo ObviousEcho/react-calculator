88a484689e72ac88ff107b2d554c2b1cfa6f7d51
Adia & Indy <107782632+ObviousEcho@users.noreply.github.com>
2023-08-28 23:08:43 -0600

    connect to jawsdb

--- server/database/database.js
+++ server/database/database.js
@@ -1,14 +1,18 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-const db = mysql.createConnection(
-  {
+let db;
+
+if (process.env.JAWSDB_URL) {
+  db = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  db = mysql.createConnection({
     host: "localhost",
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-  }
-);
+  });
+}
 
 db.connect((err) => {
   if (err) throw err;
